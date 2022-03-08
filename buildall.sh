4f5fc96ecd53b0f42b80cf36e1b062154f2c0a19
Antoni Sawicki <tenox@google.com>
2022-03-08 00:25:11 -0800

    update buildall

--- buildall.sh
+++ buildall.sh
@@ -1,1 +1,2 @@
-eval "$(go tool dist list | awk -v FS=/ -v BIN=stc '{ print "GOOS=" $1 " GOARCH=" $2 " go build -o out/" BIN "-" $2 "-" gensub(/windows/, "windows.exe", "g", $1) }' )"
+rm -f out/*
+eval "$(go tool dist list | awk -v FS=/ -v BIN=stc '!/^(plan9|ios|js|android)/ { print "GOOS=" $1 " GOARCH=" $2 " go build -o out/" BIN "-" $2 "-" gensub(/windows/, "windows.exe", "g", $1) }' )"
