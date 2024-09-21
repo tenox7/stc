8f4c316e93e6359d958a277711c43faeb8050b50
Antoni Sawicki <as@tenoware.com>
2024-09-21 12:16:02 -0700

    makefile

--- buildall.sh
+++
@@ -1,2 +0,0 @@
-rm -f out/*
-eval "$(go tool dist list | awk -v FS=/ -v BIN=stc '!/^(plan9|ios|js|android)/ { print "GOOS=" $1 " GOARCH=" $2 " go build -o out/" BIN "-" $2 "-" gensub(/windows/, "&.exe", "g", $1) }' )"
